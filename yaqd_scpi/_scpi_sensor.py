298a8d1a2445461d70a67f621ad302c5dcae3c11
Blaise Thompson <blaise@untzag.com>
2021-03-10 23:59:43 +0000

    initial sensor implementation

--- yaqd_scpi/_scpi_sensor.py
+++ yaqd_scpi/_scpi_sensor.py
@@ -1,26 +1,27 @@
-__all__ = ["ScpiSensor"]
+__all__ = ["SCPISensor"]
 
 import asyncio
 from typing import Dict, Any, List
 
-from yaqd_core import IsDaemon
+import pyvisa
+
+from yaqd_core import HasMeasureTrigger, IsSensor, IsDaemon
 
 
-class ScpiSensor(IsDaemon):
+class SCPISensor(HasMeasureTrigger, IsSensor, IsDaemon):
     _kind = "scpi-sensor"
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        # Perform any unique initialization
+        self._channel_names = list(config["channels"].keys())
+        self._channel_units = {k: config["channels"][k]["units"] for k in self._channel_names}
+        rm = pyvisa.ResourceManager("@py")  # use pyvisa-py backend
+        self._instrument = rm.open_resource(config["visa_address"])
 
-    async def update_state(self):
-        """Continually monitor and update the current daemon state."""
-        # If there is no state to monitor continuously, delete this function
-        while True:
-            # Perform any updates to internal state
-            self._busy = False
-            # There must be at least one `await` in this loop
-            # This one waits for something to trigger the "busy" state
-            # (Setting `self._busy = True)
-            # Otherwise, you can simply `await asyncio.sleep(0.01)`
-            await self._busy_sig.wait()
+    async def measure(self):
+        out = {}
+        for k in self._channel_names:
+            query = self._config["channels"][k]["query"]
+            self._instrument.write(query)
+            out[k] = float(self._instrument.read())
+        return out
