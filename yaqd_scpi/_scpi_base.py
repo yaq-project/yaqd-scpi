cf3e745a374b56cc23e933153e62852a6809165e
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2023-03-08 17:45:43 -0600

    remove context manager

--- yaqd_scpi/_scpi_base.py
+++ yaqd_scpi/_scpi_base.py
@@ -13,15 +13,13 @@ class SCPIBase(IsDaemon):
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
         if sys.platform.startswith("win32"):
-            self.rm = pyvisa.ResourceManager()  # use ni-visa backend
+            rm = pyvisa.ResourceManager()  # use ni-visa backend
         else:
-            self.rm = pyvisa.ResourceManager("@py")  # use pyvisa-py backend
-        self._loop.create_task(self.connect_instrument(config["visa_address"], timeout=1000))
-
-    async def connect_instrument(self, *args, **kwargs):
-        with self.rm.open_resource(*args, **kwargs) as instr:
-            self._instrument: pyvisa.Resource = instr
-            await asyncio.sleep(1)
+            rm = pyvisa.ResourceManager("@py")  # use pyvisa-py backend
+        self._instrument: pyvisa.Resource = rm.open_resource(config["visa_address"], timeout=1000)
 
     def direct_scpi_write(self, command: str):
         self._instrument.write(command)
+
+    def close(self):
+        self._instrument.close()
